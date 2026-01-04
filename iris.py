4ee47bdc7adf7c2adfa9922721ad0de53c51c97a
rrejinrejinr <rrejinrejin987@gmail.com>
2026-01-04 18:38:59 +0530

    Update iris.py

--- iris.py
+++ iris.py
@@ -36,10 +36,21 @@ st.write("Accuracy:", round(accuracy * 100, 2), "%")
 
 st.subheader("Predict a Flower")
 
-sepal_length = st.slider("Sepal Length", 4.0, 8.0, 5.1)
-sepal_width = st.slider("Sepal Width", 2.0, 4.5, 3.5)
-petal_length = st.slider("Petal Length", 1.0, 7.0, 1.4)
-petal_width = st.slider("Petal Width", 0.1, 2.5, 0.2)
+sepal_length = st.number_input(
+    "Sepal Length", min_value=4.0, max_value=8.0, value=5.1, step=0.1
+)
+
+sepal_width = st.number_input(
+    "Sepal Width", min_value=2.0, max_value=4.5, value=3.5, step=0.1
+)
+
+petal_length = st.number_input(
+    "Petal Length", min_value=1.0, max_value=7.0, value=1.4, step=0.1
+)
+
+petal_width = st.number_input(
+    "Petal Width", min_value=0.1, max_value=2.5, value=0.2, step=0.1
+)
 
 if st.button("Predict"):
     result = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
